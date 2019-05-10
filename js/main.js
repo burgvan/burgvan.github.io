34f2c678384f1c1cab092657668fd54939f7954e
burgvan <vanessa.burguete@gmail.com>
2019-05-10 15:08:38 -0400

    test form

--- js/main.js
+++ js/main.js
@@ -12,58 +12,58 @@
         }
     }
 
-    function emailValidation(email_address) {
-        var pattern = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
-        return pattern.test(email_address);
-    }
+    // function emailValidation(email_address) {
+    //     var pattern = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+    //     return pattern.test(email_address);
+    // }
 
-    function contactForm() {
-        var contact_form = $('#contact-form');
+    // function contactForm() {
+    //     var contact_form = $('#contact-form');
 
-        contact_form.on('submit', function (e) {
+    //     contact_form.on('submit', function (e) {
 
-            var input = $(this).find('input, textarea');
-            var required_fields = $(this).find('.required');
-            var email_field = $('.field--email');
-            var contact_name_val = $('.field--name').val();
-            var contact_email_val = email_field.val();
-            var contact_message_val = $('.field--message').val();
-            var contact_notice = $('.form--notice');
+    //         var input = $(this).find('input, textarea');
+    //         var required_fields = $(this).find('.required');
+    //         var email_field = $('.field--email');
+    //         var contact_name_val = $('.field--name').val();
+    //         var contact_email_val = email_field.val();
+    //         var contact_message_val = $('.field--message').val();
+    //         var contact_notice = $('.form--notice');
 
-            e.preventDefault();
+    //         e.preventDefault();
 
-            if (contact_name_val == '' || contact_email_val == '' || contact_message_val == '') {
-                contact_notice.stop(true).hide().html(contact_input_error).fadeIn();
-                required_fields.each(function () {
-                    $(this).addClass("input-error");
-                });
+    //         if (contact_name_val == '' || contact_email_val == '' || contact_message_val == '') {
+    //             contact_notice.stop(true).hide().html(contact_input_error).fadeIn();
+    //             required_fields.each(function () {
+    //                 $(this).addClass("input-error");
+    //             });
 
-            } else if (!emailValidation(contact_email_val)) {
-                contact_notice.stop(true).hide().html(contact_email_error).fadeIn();
-                email_field.addClass("input-error");
-                $('#email').focus();
-            }
-            else {
-                $.ajax({
-                    type: 'POST',
-                    url: '../php/contact.php',
-                    data: {
-                        name: contact_name_val,
-                        email: contact_email_val,
-                        message: contact_message_val,
-                        emailAddress: contact_email
-                    },
-                    success: function () {
-                        contact_notice.stop(true).hide().html(contact_success).fadeIn();
-                        contact_form[0].reset();
-                        input.blur();
-                    }
-                });
-            }
-            return false;
+    //         } else if (!emailValidation(contact_email_val)) {
+    //             contact_notice.stop(true).hide().html(contact_email_error).fadeIn();
+    //             email_field.addClass("input-error");
+    //             $('#email').focus();
+    //         }
+    //         else {
+    //             $.ajax({
+    //                 type: 'POST',
+    //                 url: '../php/contact.php',
+    //                 data: {
+    //                     name: contact_name_val,
+    //                     email: contact_email_val,
+    //                     message: contact_message_val,
+    //                     emailAddress: contact_email
+    //                 },
+    //                 success: function () {
+    //                     contact_notice.stop(true).hide().html(contact_success).fadeIn();
+    //                     contact_form[0].reset();
+    //                     input.blur();
+    //                 }
+    //             });
+    //         }
+    //         return false;
 
-        });
-    }
+    //     });
+    // }
 
     $(window).scroll(function(event){
         hasScrolled();
